63b1cf98d8122f25c9edbb3db360e4472c0865ba
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2018-04-06 22:05:01 -0700

    added scroll function for navbar

--- Raymond Martinez' Resume/JS/resume.js
+++ Raymond Martinez' Resume/JS/resume.js
@@ -1,17 +1,63 @@
 
-$(document).ready(function() {
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 2000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
-})
+/*
+$('a[href^="#"]').on('click', function(event) {
 
-window.scroll({
-  top: 2500, 
-  left: 0, 
-  behavior: 'smooth' 
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 3000);
+    }
+
 });
 
-function scrollTo() {
-	document.querySelector('.two').scrollIntoView({ behavior: 'smooth' });
-};
+
+$('ul').find('a').click(function(){
+    var $href = $(this).attr('href');
+    var $anchor = $('#'+$href).offset();
+	$('body').animate({ scrollTop: $anchor.top }, 3000);
+    return false;
+});
+*/
 
 $(document).ready(function() {
 	$('#previous').on('click', function() {
