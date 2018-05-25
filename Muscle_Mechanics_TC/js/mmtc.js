9cf91b2f4d9ae7db6b728d5bb67a4c59d74b1f02
Jake Howard <bsidedrums@Jake-Howards-MacBook-Pro.local>
2018-05-24 21:37:00 -0700

    changed background color for .two on purpose.html and cleaned up .js

--- Muscle_Mechanics_TC/js/mmtc.js
+++ Muscle_Mechanics_TC/js/mmtc.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function() {
 
+// All scroll functions for pages
+
 	$(window).scroll(function() {
     	if ($(document).scrollTop() > 700) {
         	$(".left_side").fadeIn(800);
@@ -50,67 +52,6 @@ $(document).ready(function() {
     }
 
     rotateTest();
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 3250) {
-    //    	$('#ads_left').removeClass('show_ads_left').addClass('hide_ads_left');
-    //    	$('#ads_right').removeClass('show_ads_right').addClass('hide_ads_right');
-    //    } 
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 800) {
-    //    	$('#ads_left').removeClass('hide_ads_left').addClass('show_ads_left');
-    //    } else if ($(window).scrollTop() < 800) {
-    //    	$('#ads_left').removeClass('show_ads_left').addClass('hide_ads_left');
-    //    }
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 3250) {
-    //    	$('#ads_left').removeClass('show_ads_left').addClass('hide_ads_left');
-    //    } 
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 800) {
-    //    	$('#ads_right').removeClass('hide_ads_right').addClass('show_ads_right');
-    //    } else if ($(window).scrollTop() < 800) {
-    //    	$('#ads_right').removeClass('show_ads_right').addClass('hide_ads_right');
-    //    }
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 3250) {
-    //    	$('#ads_right').removeClass('show_ads_right').addClass('hide_ads_right');
-    //    } 
-    // });
-
-    // $(window).scroll(function() {
-    // 	if ($(document).scrollTop() > 900) {
-    // 		$('#two').addClass('two_change');		
-    // 	}
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 780) {
-    //     $("header").fadeIn(600);
-    //    } else if ($(document).scrollTop() < 700) {
-    //    	$("header").fadeOut(600);
-    //    }
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 700) {
-    //     $(".left_side").fadeIn(800);
-    //    } 
-    // });
-
-    // $(window).scroll(function() {
-    //    if ($(document).scrollTop() > 1640) {
-    //     $(".info_imgs").fadeIn(800);
-    //    } 
-    // });
 
 })
 
